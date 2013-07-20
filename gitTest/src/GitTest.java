3732eea200b37d43ab12464fe3eb5a9b9a309bb5
joe3701 <joe3701@gmail.com>
2013-07-20 08:04:14 -0700

    Initializing java project

---
+++ gitTest/src/GitTest.java
@@ -0,0 +1,9 @@
+
+public class GitTest {
+
+	public static void main(String[] args) {
+		System.out.println("This is a test of the functionality of git and linking to gitHub.");
+		System.out.println("It is quite interesting to use!!");
+	}
+
+}
